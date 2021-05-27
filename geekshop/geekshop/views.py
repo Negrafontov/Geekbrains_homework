0cf4724134cd01078286bb03a61fe3248ff17f4e
Andrew <negrafontov.andrew@gmail.com>
2021-05-27 15:29:04 +0300

    les2

---
+++ geekshop/geekshop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    title = 'главная'
+
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'geekshop/index.html', context=context)
+
+
+def contacts(request):
+    title = 'контакты'
+
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'geekshop/contact.html', context=context)
